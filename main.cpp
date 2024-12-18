232c87abeb4bfda31bc17495a85049a727b6fdfe
Marc Blanc Sans <marc.blanc@estudiantat.upc.edu>
2024-12-18 17:50:22 +0100

    notcurses linked

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <notcurses/notcurses.h>
 
 int main(int, char**){
-    std::cout << "Hello, from MagicTerminal!\n";
+    //notcurses_options* ncurses_opt;
+    
+    struct notcurses* notcurses = notcurses_init(NULL, stdout);
+    
+    if (!notcurses) {
+        std::cout << "Failed To initialize NotCurses\n";
+    }
+    const char* ncurses_version = notcurses_version();
+    std::cout << "NotCurses version: " << ncurses_version << '\n';
+    notcurses_stop(notcurses);
+    return 0;
 }
